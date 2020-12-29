e73d977a7044ef3da4a266f66fe370cd2c701ef7
tamaji22 <tamaji229+github@gmail.com>
2020-12-30 08:42:06 +0900

    feat: 画面のデザインを整えるために、Blumaでの編集をした

--- my-project/src/components/BasicInfo.vue
+++ my-project/src/components/BasicInfo.vue
@@ -1,33 +1,49 @@
 <template>
-  <div>
-    <div>
-      <p>STEP1</p>
-      <p>お客様の情報を入力してください</p>
+    <section class="section">
+    <div class="container">
+      <div class="card">
+        <div class="card-content has-background-link p-3">
+          <span class="tag is-info is-pulled-left">STEP1</span>
+          <p class="has-text-centered is-size-5 has-text-light"><font-awesome-icon icon="address-card" class="mr-2"/>お客様の情報を入力してください</p>
+        </div>
+        <div class="card-content">
+          <p class="has-text-info mb-2">-性別-</p>
+          <div class="control">
+            <label class="radio">
+              <input type="radio" name="gender" value="male"><div class="is-inline-block ml-1">男性</div>
+            </label>
+            <label class="radio">
+              <input type="radio" name="gender" value="female"><div class="is-inline-block ml-1">女性</div>
+            </label>
+          </div>
+          <div class="mt-5 mb-3">
+            <p class="has-text-info mb-2">-生年月日-</p>
+            <div class="select">
+              <select name="year">
+                <option v-for="i in 100" :key="i" value="year + i">{{ year + i }}年{{ wareki(year + i) }}</option>
+              </select>
+            </div>
+            <div class="is-inline-block pt-2 pl-1 pr-3">年</div>
+            <div class="select">
+              <select name="month">
+                <option v-for="i in 12" :key="i" value="i">{{ i }}</option>
+              </select>
+            </div>
+            <div class="is-inline-block pt-2 pl-1 pr-3">月</div>
+            <div class="select">
+              <select name="day">
+                <option v-for="i in 31" :key="i" value="i">{{ i }}</option>
+              </select>
+            </div>
+            <div class="is-inline-block pt-2 pl-1">日</div>
+          </div>
+        </div>
+      </div>
+      <div class="has-text-centered mt-6">
+        <button class="button is-primary" @click="nextPage">次へ進む<font-awesome-icon icon="angle-right" class="ml-3"/></button>
+      </div>
     </div>
-    <div>
-      <p>-性別-</p>
-      <input type="radio" name="gender" value="male">男性
-      <input type="radio" name="gender" value="female">女性
-    </div>
-    <div>
-      <p>-生年月日-</p>
-      <select name="year">
-        <option v-for="i in 100" :key="i" value="year + i">{{ year + i }}年{{ wareki(year + i) }}</option>
-      </select>
-      年
-      <select name="month">
-        <option v-for="i in 12" :key="i" value="i">{{ i }}</option>
-      </select>
-      月
-      <select name="day">
-        <option v-for="i in 31" :key="i" value="i">{{ i }}</option>
-      </select>
-      日
-    </div>
-    <div>
-      <button @click="nextPage">次へ進む</button>
-    </div>
-  </div>
+  </section>
 </template>
 
 <script>
--- my-project/src/main.js
+++ my-project/src/main.js
@@ -1,7 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/router.js'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(fas)
 
 createApp(App)
   .use(router)
+  .component('font-awesome-icon', FontAwesomeIcon)
   .mount('#app');
